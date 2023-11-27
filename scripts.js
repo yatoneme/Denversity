72a2c37a164ef1ef373e311161912f5760dcc448
Mariam Khaled <m.elkholy2004@gmail.com>
2023-11-28 01:50:01 +0200

    initiated js

--- scripts.js
+++ scripts.js
@@ -16,6 +16,7 @@ function login() {
         alert("Wrong Email and Password");
     }
 }
+
 document.getElementById("name").value = "";
 document.getElementById("email").value = "";
 document.getElementById("number").value = "";
@@ -24,3 +25,33 @@ document.getElementById("check").value = "";
 document.getElementById("uni").value = "";
 document.getElementById("notes").value = "";
 document.getElementById("timee").value="";
+
+function popup(){
+alert('Submitted Succesfully')
+}
+
+var data=[];
+var n=1;
+var x=0;
+function sendform(){ //should send form data to doctors.html and display it in a paragraph in the pending column
+var addcell=document.getElementById('form')
+var fullname=document.getElementById("name")
+var email=document.getElementById("email")
+var phone=document.getElementById("number")
+var bday=document.getElementById("bd")
+var gender=document.getElementById("check")
+var time=document.getElementById("timee")
+var uni=document.getElementById("uni")
+var notes=document.getElementById("notes")
+}
+function showapptname(){ //should show case name on top of the booking page
+   //windowlocation.href="/categories.html"+ev.target.innerhtml
+}
+function acceptappt(){ //checkmark for accepting appts
+
+}
+function completeappt(){ //checkmark for completion
+
+}
+// optional functions
+// deleteappt()
